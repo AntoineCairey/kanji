257a7860e4884bd99ba05bc7079fde6a34ceb21c
Antoine Cairey <cairey.antoine@gmail.com>
2025-02-25 23:27:42 +0100

    cors seems to work

--- backend/src/main/java/com/antoinecairey/kanji/backend/auth/SecurityConfig.java
+++ backend/src/main/java/com/antoinecairey/kanji/backend/auth/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,6 +22,7 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
+        .cors(Customizer.withDefaults())
         .csrf(csrf -> csrf.disable()) // Désactive CSRF pour API REST
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Pas de session
         .authorizeHttpRequests(auth -> auth
