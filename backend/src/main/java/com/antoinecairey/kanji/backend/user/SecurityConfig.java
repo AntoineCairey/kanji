2b98b699e310c40762bd98ddf9d5fdd33a636e3c
Antoine Cairey <cairey.antoine@gmail.com>
2025-02-25 16:07:12 +0100

    add protected routes

--- backend/src/main/java/com/antoinecairey/kanji/backend/user/SecurityConfig.java
+++ backend/src/main/java/com/antoinecairey/kanji/backend/user/SecurityConfig.java
@@ -5,28 +5,38 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+  private final JwtFilter jwtFilter;
+
+  public SecurityConfig(JwtFilter jwtFilter) {
+    this.jwtFilter = jwtFilter;
+  }
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
         .csrf(csrf -> csrf.disable()) // Désactive CSRF pour API REST
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Pas de session
         .authorizeHttpRequests(auth -> auth
             .requestMatchers("/auth/**").permitAll() // Autorise login & inscription
             .anyRequest().authenticated() // Toutes les autres routes sont protégées
-        );
+        )
+        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class); // Ajoute notre filtre JWT
 
     return http.build();
   }
 
   @Bean
   public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder(); // Hashage des mots de passe
+    return new BCryptPasswordEncoder();
   }
 
   @Bean
