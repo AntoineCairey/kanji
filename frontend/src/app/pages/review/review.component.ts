073956906c624952fc518008e40df53020876e7c
Antoine Cairey <cairey.antoine@gmail.com>
2025-02-28 15:26:27 +0100

    review with 2 steps

--- frontend/src/app/pages/review/review.component.ts
+++ frontend/src/app/pages/review/review.component.ts
@@ -2,11 +2,12 @@ import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { CardService } from '../../services/card/card.service';
 import { HttpClient } from '@angular/common/http';
-import { TileComponent } from '../../components/tile/tile.component';
+import { MatButtonModule } from '@angular/material/button';
+import { Card } from '../../models/card.model';
 
 @Component({
   selector: 'app-review',
-  imports: [TileComponent],
+  imports: [MatButtonModule],
   templateUrl: './review.component.html',
   styleUrl: './review.component.css',
 })
@@ -17,17 +18,24 @@ export class ReviewComponent implements OnInit {
   private cardService = inject(CardService);
 
   cards = this.cardService.cards;
-  currentIndex = signal(0);
-  card = computed(() => this.cards()[this.currentIndex()]);
+  currentIndex = signal<number>(0);
+  card = computed<Card>(() => this.cards()[this.currentIndex()]);
+
+  isAnswer = signal<boolean>(false);
 
   ngOnInit(): void {
     this.cardService.fetchCards();
+  }
+
+  showAnswer() {
+    this.isAnswer.set(true);
   }
 
   reviewCard(cardId: number, isSuccess: boolean) {
     this.http
       .put(`${this.BASE_URL}/review/${cardId}?success=${isSuccess}`, {})
       .subscribe((_) => {
+        this.isAnswer.set(false);
         if (this.currentIndex() < this.cards().length - 1) {
           this.currentIndex.update((ci) => ci + 1);
         } else {
