072f3cdef628a2dacb4b29b067b978212f2167eb
Antoine Cairey <cairey.antoine@gmail.com>
2025-04-03 15:39:19 +0200

    setup angular unit tests part1

--- frontend/src/app/services/kanji/kanji.service.spec.ts
+++ frontend/src/app/services/kanji/kanji.service.spec.ts
@@ -1,13 +1,25 @@
 import { TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
 import { KanjiService } from './kanji.service';
 
 describe('KanjiService', () => {
   let service: KanjiService;
+  let httpTesting;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        KanjiService,
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
+    });
     service = TestBed.inject(KanjiService);
+    httpTesting = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
